65b60f3ab4d5432b7d98bee3998c415f029b6f8a
Kevin Xu <kevin@imkevinxu.com>
2011-10-27 15:58:18 -0700

    hello world page

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 Classzy
 =======
+
+Nyan nyan nyan
+
+Initial Steps
+-------------
+	python manage.py syncdb
+	
+- Change TEMPLATE_DIRS path to your absolute path
--- settings.py
+++ settings.py
@@ -100,12 +100,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'classzy.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	"/Users/kevinxu/Projects/classzy/templates"
 )
 
 INSTALLED_APPS = (
@@ -116,9 +117,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging

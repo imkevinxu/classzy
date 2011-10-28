c4b7f5af8939dec52519b675b58a659bd3820598
Kevin Xu <kevin@imkevinxu.com>
2011-10-28 08:28:20 -0700

    test

--- settings.py
+++ settings.py
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	"/Users/kevinxu/Projects/classzy/templates"
+	"/classzy/templates"
 )
 
 INSTALLED_APPS = (

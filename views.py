b273003b791f2a1c521e985462fae7e509c49330
Kevin Xu <kevin@imkevinxu.com>
2011-11-04 03:55:03 -0700

    updated homepage

--- views.py
+++ views.py
@@ -2,9 +2,13 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanen
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.context_processors import csrf
 from django.template import RequestContext
+from classes.models import Class
 
 def home(request):
+	if request.method == 'POST':
+		try:
+			classzy = Class.objects.get(code=request.POST['class'])	
+			return render_to_response('index.html', {'class' : classzy}, context_instance=RequestContext(request))
+		except:
+			return render_to_response('index.html', {'warning' : "Sorry, class code not found" }, context_instance=RequestContext(request))
 	return render_to_response('index.html', context_instance=RequestContext(request))
-	
-def profile(request):
-	return render_to_response('profile.html', context_instance=RequestContext(request))

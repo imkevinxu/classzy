96a59bee641964c1d8fdd71535a8a938f825138e
Kevin Xu <kevin@imkevinxu.com>
2011-11-04 03:24:43 -0700

    playing with static url

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
@@ -13,7 +14,6 @@ urlpatterns = patterns('',
     url(r'^$', 'views.home'),
 	url(r'^profile', 'views.profile'),
 	
-	# Static URL
-	(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-
 )
+
+urlpatterns += staticfiles_urlpatterns()

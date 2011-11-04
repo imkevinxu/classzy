c6cb2f23e0d794d02b986fabb80d83e074116d45
Kevin Xu <kevin@imkevinxu.com>
2011-11-04 03:28:23 -0700

    optional prof

--- classes/models.py
+++ classes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Class(models.Model):
 	code = models.CharField(max_length=20)
 	name = models.CharField(max_length=100)
-	professor = models.CharField(max_length=100, null=True)
+	professor = models.CharField(max_length=100, blank=True, null=True)
 	
 	def __unicode__(self):
 		return u'%s - %s' % (self.code, self.name)

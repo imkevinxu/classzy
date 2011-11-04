f05d56ce8a30c146f2f919ecf680380f6b6bf64a
Kevin Xu <kevin@imkevinxu.com>
2011-11-04 05:20:39 -0700

    completed add class function

--- classes/models.py
+++ classes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Class(models.Model):
-	code = models.CharField(max_length=20)
+	code = models.CharField(max_length=20, primary_key=True)
 	name = models.CharField(max_length=100, blank=True, null=True)
 	professor = models.CharField(max_length=100, blank=True, null=True)
 	
--- views.py
+++ views.py
@@ -6,9 +6,18 @@ from classes.models import Class
 
 def home(request):
 	if request.method == 'POST':
-		try:
-			classzy = Class.objects.get(code=request.POST['class'])	
-			return render_to_response('index.html', {'classzy' : classzy}, context_instance=RequestContext(request))
-		except:
-			return render_to_response('index.html', {'warning' : "Sorry, class code not found" }, context_instance=RequestContext(request))
+		if 'class' in request.POST:
+			try:
+				classzy = Class.objects.get(code=request.POST['class'])	
+				return render_to_response('index.html', {'classzy' : classzy}, context_instance=RequestContext(request))
+			except:
+				return render_to_response('index.html', {'warning' : "Sorry, class code not found" }, context_instance=RequestContext(request))
+		elif 'class_code' in request.POST:
+			try:
+				classzy = Class.objects.get(code=request.POST['class_code'])	
+				return render_to_response('index.html', {'adding_warning' : "That class is already in Classzy!" }, context_instance=RequestContext(request))
+			except:
+				classzy = Class(code=request.POST['class_code'])
+				classzy.save()
+				return render_to_response('index.html', {'classzy' : classzy}, context_instance=RequestContext(request))
 	return render_to_response('index.html', context_instance=RequestContext(request))
